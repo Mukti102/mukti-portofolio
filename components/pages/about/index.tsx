3e15584ed7410f01ee866f493a687488aec6d046
unknown <abdulmukti40390@gmail.comgit config --global.user.mukti>
2024-05-30 13:26:34 +0700

    add download cv feature

--- components/pages/about/index.tsx
+++ components/pages/about/index.tsx
@@ -1,16 +1,31 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import ai from "@/public/assets/ai.png";
+const pdf = "http://localhost:3000/mukti.pdf";
+
 function About() {
+  const downloadCv = (url: string) => {
+    if (typeof document !== "undefined") {
+      const fileName: any = url.split("/").pop();
+      const aTag = document.createElement("a");
+      aTag.href = url;
+      aTag.setAttribute("download", fileName);
+      document.body.append(aTag);
+      aTag.click();
+      document.body.removeChild(aTag);
+    }
+  };
+
   return (
-    <div className="w-full  md:flex-row-reverse flex-col pt-10 mb-10 md:mt-0 mt-10 font-bold md:flex items-center h-screen gap-24 px-5 md:px-20">
+    <div className="w-full md:flex-row-reverse flex-col pt-10 mb-10 md:mt-0 mt-10 font-bold md:flex items-center h-screen gap-24 px-5 md:px-20">
       <div
         data-aos="fade-up"
         data-aos-duration="1500"
         data-aos-easing="ease-in-out"
-        className="flex-1 touch-element  h-max leading-[50px]"
+        className="flex-1 touch-element h-max leading-[50px]"
       >
-        <h1 className="font-bold mb-0  md:mb-5 text-[2.5rem] md:text-[3rem] text-black">
+        <h1 className="font-bold mb-0 md:mb-5 text-[2.5rem] md:text-[3rem] text-black">
           About <span className="text-pink-500">Me</span>
         </h1>
         <span className="md:text-xl text-lg text-pink-500">
@@ -23,18 +38,21 @@ function About() {
           consectetur vitae laboriosam?
         </p>
         <div className="my-5">
-          <span className="md:px-12 px-9 py-2 bg-gray-50 rounded-md text-gray-800 md:py-3 md:text-lg text-sm out-shadow">
+          <button
+            onClick={() => downloadCv(pdf)}
+            className="md:px-12 px-9 py-2 bg-gray-50 rounded-md text-gray-800 md:py-3 md:text-lg text-sm out-shadow"
+          >
             Download CV
-          </span>
+          </button>
         </div>
       </div>
       <div
         data-aos="fade-right"
         data-aos-duration="1500"
         data-aos-easing="ease-in-out"
-        className="w-max  mx-auto"
+        className="w-max mx-auto"
       >
-        <div className="out-shadow p-10 md:p-10 rounded-md md:rounded-tr-[200px] hover:rounded-md  md:hover:rounded-tl-[200px] hover:rounded-tr-none flex justify-center transition-all duration-200 ease-out md:w-max">
+        <div className="out-shadow p-10 md:p-10 rounded-md md:rounded-tr-[200px] hover:rounded-md md:hover:rounded-tl-[200px] hover:rounded-tr-none flex justify-center transition-all duration-200 ease-out md:w-max">
           <Image
             width={200}
             height={200}
